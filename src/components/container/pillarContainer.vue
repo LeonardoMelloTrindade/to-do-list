98f41a143d469a19d00e8338558f47beb9a05265
Leonardo Mello Trindade <leonardomellotrindade@gmail.com>
2024-11-10 15:13:44 -0300

    feat: adiciona container global

---
+++ src/components/container/pillarContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="grid-pillar">
+    <PillarLayout position="right"/>
+    <slot>aqui fica o componente de cada centerContent de cada pages</slot>
+    <PillarLayout position="left"/>
+  </main>
+</template>
+
+<script setup lang="ts">
+import PillarLayout from '../layout/pillarLayout.vue';
+
+
+
+</script>
+
+<style scoped>
+.grid-pillar {
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+  height: 100%;
+}
+</style>
---
+++ src/components/layout/pillarLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :position="position"/>
+</template>
+
+<script setup lang="ts">
+const { position } = defineProps(['position']);
+
+const cssPosistioShadow = position == 'right' ? '10px' : '-10px';
+
+</script>
+
+<style scoped>
+div {
+  background-image: url('../../assets/imagePillar.jpg');
+  background-repeat: repeat;
+  background-size: cover;
+  height: 100%;
+  box-shadow: v-bind('cssPosistioShadow') 0px 10px 1px #7e7e7e;
+}
+
+</style>
