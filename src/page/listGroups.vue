07197eab6130e5679665df732c3795d7901af0a7
Leonardo Mello Trindade <leonardomellotrindade@gmail.com>
2024-11-10 19:14:08 -0300

    feat: adiciona pagina de grupos

---
+++ src/page/listGroups.vue
@@ -0,0 +1,29 @@
+<template>
+  <ContainerMain>
+    <template #main>
+      <CenterContent>
+        <template #title>
+          <TitleHeader title="Leopple To do" />
+        </template>
+        <template #button>
+          <BtnAdd label=" + Criar Grupo "/>
+        </template>
+        <template #list>
+          oi
+        </template>
+      </CenterContent>
+    </template>
+  </ContainerMain>
+</template>
+
+<script setup lang="ts">
+import ContainerMain from '@/components/container/containerMain.vue';
+import TitleHeader from '@/components/ui/titleHeader.vue';
+import CenterContent from '@/components/layout/centerContent.vue';
+import BtnAdd from '@/components/ui/btnAdd.vue';
+
+</script>
+
+<style scope>
+h1 {}
+</style>
