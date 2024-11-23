e3e5e18653056851ba31ca2a9e94b07c063de396
Leonardo Mello Trindade <leonardomellotrindade@gmail.com>
2024-11-23 15:25:06 -0300

    feat: finaliza adicao de estizacao do button

--- src/components/ui/btnAdd.vue
+++ src/components/ui/btnAdd.vue
@@ -1,19 +1,11 @@
 <template>
-  <Button icon="pi pi-plus" :label="label" severity="primary"/>
+  <Button severity="secondary" icon="pi pi-plus" :label="label" raised/>
 </template>
 
 <script setup lang="ts">
-// import { theme } from '@/styles/theme';
 import { Button } from 'primevue';
 
 const { label } = defineProps(['label']);
-
-
 </script>
 
-<style scoped>
-button:hover {
-  box-shadow: 3px 5px 1px #7e7e7e;
-  transition: 0.5s;
-}
-</style>
+
