1d9f27c8d7bc35b723dc6d76dfce9c9e1013fc35
Leonardo Mello Trindade <leonardomellotrindade@gmail.com>
2024-11-23 22:17:26 -0300

    feat: finaliza adicao de componente tabs

--- src/components/ui/ContainerList.vue
+++ src/components/ui/ContainerList.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="container-list">
-    <nav class="navigation" v-if="itensNavigation?.length !== 0">
+    <!-- <nav class="navigation" v-if="itensNavigation?.length !== 0">
       <span v-for="item of itensNavigation" :key="item" class="itens-navigation">
         {{ item }}
       </span>
@@ -9,19 +9,27 @@
       <span class="itens-navigation">{{ itensNavigation[0] }}</span>
     </nav>
     <section class="list">
-      <ListGroupItem  name="Faculdade" alltasks="10" alltasks-finished="5e"/>
-      <ListGroupItem  name="Trabalho" alltasks="10" alltasks-finished="11"/>
-    </section>
+    </section> -->
+    <NavContainerList :itensNavigation="itensNavigation">
+      <template #session>
+        <ListGroupItem  name="Faculdade" alltasks="10" alltasks-finished="5"/>
+        <ListGroupItem  name="Trabalho" alltasks="10" alltasks-finished="11"/>
+      </template>
+      <template #allTasks>
+        <div>Tasks</div>
+      </template>
+    </NavContainerList>
+
   </main>
 </template>
 
 <script setup lang="ts">
 import ListGroupItem from '@/components/ui/ListGroupItem.vue';
+import NavContainerList from './NavContainerList.vue';
 
 const { itensNavigation } = defineProps({
   itensNavigation: Array<string>,
 });
-
 </script>
 
 <style scoped>
@@ -29,7 +37,7 @@ const { itensNavigation } = defineProps({
   min-width: 450px;
   width: 80%;
   border: 1px solid;
-  background-color: #444444;
+  background-color: #18181b;
   min-height: 25rem;
   border-radius: 1.5em;
 }
---
+++ src/components/ui/NavContainerList.vue
@@ -0,0 +1,37 @@
+<template>
+    <Tabs value="0">
+      <TabList>
+        <div class="navigation">
+          <Tab :value="index.toString()" v-for="item, index of itensNavigation" :key="item">{{ item }}</Tab>
+        </div>
+      </TabList>
+      <TabPanels class="list">
+        <TabPanel value="0">
+          <slot name="session"></slot>
+        </TabPanel>
+        <TabPanel value="1">
+          <slot name="allTasks"></slot>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+</template>
+
+<script setup lang="ts">
+import { Tabs, Tab, TabList, TabPanel, TabPanels } from 'primevue';
+
+
+const { itensNavigation } = defineProps({
+  itensNavigation: Array<string>,
+});
+</script>
+
+<style scoped>
+.navigation {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.list {
+  margin-top: 3rem;
+}
+</style>
