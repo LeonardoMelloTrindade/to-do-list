33f6e487d53e5464646cc4e381b7a687ff47446f
Leonardo Mello Trindade <leonardomellotrindade@gmail.com>
2025-01-02 20:01:47 -0300

    feat: ajusta identacao

--- src/components/ui/BreadCrumb.vue
+++ src/components/ui/BreadCrumb.vue
@@ -8,7 +8,8 @@
                         <span class="text-primary font-semibold ">{{ item.label }}</span>
                     </a>
                 </router-link>
-                    <a v-else :href="item.url" :target="item.target" v-bind="props.action">                            <span class="text-color">{{ item.label }}</span>
+                    <a v-else :href="item.url" :target="item.target" v-bind="props.action">                            
+                        <span class="text-color">{{ item.label }}</span>
                     </a>
             </template>
         </Breadcrumb>
