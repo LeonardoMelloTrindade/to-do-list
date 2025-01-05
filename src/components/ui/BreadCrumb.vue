93010b83a01799d17cd1ebabb0f6c1e09b36244c
Leonardo Mello Trindade <leonardomellotrindade@gmail.com>
2025-01-04 22:32:41 -0300

    feat: adiciona dinamismo para o path do breadcrumb

--- src/components/ui/BreadCrumb.vue
+++ src/components/ui/BreadCrumb.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Breadcrumb class="breadcrumb-background" :home="home" :model="items">
+        <Breadcrumb class="breadcrumb-background" :home="home" :model="path">
             <template  #item="{ item, props }">
                 <router-link  v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
                     <a class="link-router" :href="href" v-bind="props.action" @click="navigate">
@@ -17,18 +17,20 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, type PropType } from 'vue';
 import Breadcrumb from 'primevue/breadcrumb';
+import { type PathBreadcrumb } from '@/interfaces/PathBreadcrumb';
 
-const { groupId, categoryId } = defineProps(['groupId', 'categoryId']);
+const { path } = defineProps({
+    path: {
+        type: Array as PropType<PathBreadcrumb[]>, 
+        required: true,
+    },
+});
 const home = ref({
     icon: 'pi pi-home',
     route: '/',
 });
-const items = ref([
-    { label: `Grupo #${groupId}`, route: `/group/${groupId}`},
-    { label: `Categoria ${categoryId}`, route: `/category/${categoryId}`},
-]);
 </script>
 
 <style scoped>
--- src/pages/GroupPage.vue
+++ src/pages/GroupPage.vue
@@ -1,33 +1,33 @@
 <template>
-    <ContainerMain>
-      <template #main>
-        <CenterContent>
-          <template #title>
-            <div class="breadcrumb">
-              <Breadcrumb />
-            </div>
-            <TitleHeader :title="groupFound?.name" />
-          </template>
-          <template #button>
-            <BtnAdd label="Categoria"/>
-          </template>
-          <template #list>
-            <div v-if="groupFound">
-              <ContainerList
-                tab="Categorias"
-                :category="groupFound.categories"
-                :tasks="groupFound.tasks"
-              />
-            </div>
-              <div v-else>
-                <p>Grupo não acessível...</p>
-              </div>
-          </template>
-        </CenterContent>
-      </template>
-    </ContainerMain>
-  </template>
-  
+  <ContainerMain>
+    <template #main>
+      <CenterContent>
+        <template #title>
+          <div class="breadcrumb">
+            <Breadcrumb :path="path ? [path] : []"/>
+          </div>
+          <TitleHeader :title="groupFound?.name" />
+        </template>
+        <template #button>
+          <BtnAdd label="Categoria"/>
+        </template>
+        <template #list>
+          <div v-if="groupFound">
+            <ContainerList
+              tab="Categorias"
+              :category="groupFound.categories"
+              :tasks="groupFound.tasks"
+            />
+          </div>
+          <div v-else>
+            <p>Grupo não acessível...</p>
+          </div>
+        </template>
+      </CenterContent>
+    </template>
+  </ContainerMain>
+</template>
+
 <script setup lang="ts">
 import TitleHeader from '@/components/ui/TitleHeader.vue';
 import ContainerList from '@/components/ui/ContainerList.vue';
@@ -39,27 +39,30 @@ import { onBeforeMount, ref } from 'vue';
 import { useGroupStore } from '@/store/GroupStore';
 import { useRoute } from 'vue-router';
 import { type Group } from '@/interfaces/Group';
+import { type PathBreadcrumb } from '@/interfaces/PathBreadcrumb';
 
 const route = useRoute();
 const group = useGroupStore();
 const groupFound = ref<Group>();
+const path = ref<PathBreadcrumb | undefined>(undefined);
 
 onBeforeMount(() => {
   groupFound.value = group.findGroupById(route.params.id)!;
   if (!groupFound.value) {
     throw new Error(`Group with ID ${route.params.id} not found`);
   }
+  path.value = { label: groupFound.value?.name, route: `/group/${groupFound.value?.id}` };
 });
 </script>
-  
-<style scoped>
-    .breadcrumb {
-      display: flex;
-      justify-content: flex-start;
-      width: 100%;
-    }
 
-    p {
-      color: black;
-    }
+<style scoped>
+.breadcrumb {
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+}
+
+p {
+  color: black;
+}
 </style>
