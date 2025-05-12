b8ff911223ff397de7a1c55b1c5db5cd34f6d19b
Leonardo Mello Trindade <leonardomellotrindade@gmail.com>
2025-05-12 14:54:52 -0300

    test: remove .only em test

--- src/components/ui/ModalCreateSession.test.ts
+++ src/components/ui/ModalCreateSession.test.ts
@@ -2,7 +2,7 @@ import { expect, describe, it } from 'vitest';
 import { customMount } from '@/tests/utils';
 import ModalCreateSession from './ModalCreateSession.vue';
 
-describe.only('ModalCreateSession.vue', () => {
+describe('ModalCreateSession.vue', () => {
   it('should render ModalCreateSession', () => {
     const wrapper = customMount(ModalCreateSession);
 
